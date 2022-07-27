ed4216b30e204477b3e1742bf41380b4fc53038f
darrienjohnson <darrien_johnson@comcast.com>
2022-07-26 22:45:41 -0400

    correct ready for launch display

--- script.js
+++ script.js
@@ -18,6 +18,8 @@ window.addEventListener("load", function(){
    let copilotStatus = document.getElementById('copilotStatus');
    let fuelStatus = document.getElementById('fuelStatus');
    let cargoStatus = document.getElementById('cargoStatus');
+   let launchStatus = document.getElementById('launchStatus');
+   
 
    // validate number for fuel level and cargo mass input field
    function validateNumber(number) {
@@ -47,19 +49,29 @@ window.addEventListener("load", function(){
 
       pilotStatus.innerHTML = pilotInput.value
       copilotStatus.innerHTML = copilotInput.value
-
+    //If shuttle fuel too low display a red warning
       if(fuelLevelInput.value < 10000){
          fuelStatus.style.color= "red";
+         launchStatus.style.color= "red";
          fuelStatus.innerHTML = `NOT ENOUGH FUEL FOR SHUTTLE TO TAKE OFF!`;
+         launchStatus.innerHTML = `Shuttle not ready for launch`
       }else {fuelStatus.innerHTML = `Fuel Level Check: Passed`;
-      fuelStatus.style.color = "inherit"}
-
+      fuelStatus.style.color = "inherit"
+      }
+   //if shuttle mass too heavy display a red warning
       if(cargoMassInput.value > 10000){
          cargoStatus.style.color= "red";
+         launchStatus.style.color= "red";
          cargoStatus.innerHTML = `TOO MUCH MASS FOR SHUTTLE TO TAKE OFF!`;
+         launchStatus.innerHTML = `Shuttle not ready for launch`
       }else {cargoStatus.innerHTML = `Cargo Mass Check: Passed`;
-      cargoStatus.style.color = "inherit"}
-
+      cargoStatus.style.color = "inherit"
+   //if shuttle fuel and mass level are correct range ready for launch  
+      if(fuelLevelInput.value > 10000 && cargoMassInput.value < 10000){
+         
+         launchStatus.innerHTML = `Shuttle is ready for launch`
+         launchStatus.style.color = "green"}
+      }
       
          
    event.preventDefault();
