097db49e80d50f26ec8e7a9c52d4e276685a7623
darrienjohnson <darrien_johnson@comcast.com>
2022-07-29 11:34:10 -0400

    refactoring

--- script.js
+++ script.js
@@ -1,17 +1,5 @@
 window.addEventListener("load", function(){
    let form = document.getElementById("launchForm");
-
-   let pilotInput = document.getElementById('pilotName');
-   let copilotInput = document.getElementById('copilotName');
-   let fuelLevelInput = document.getElementById("fuelLevel");
-   let cargoMassInput = document.getElementById("cargoMass");
-   
-   let pilotStatus = document.getElementById('pilotStatus');
-   let copilotStatus = document.getElementById('copilotStatus');
-   let fuelStatus = document.getElementById('fuelStatus');
-   let cargoStatus = document.getElementById('cargoStatus');
-   let launchStatus = document.getElementById('launchStatus');
-
    //button to submit form
    let submit = document.getElementById('submit');
 
@@ -64,88 +52,84 @@ window.addEventListener("load", function(){
    
    //Function to Validate inputs of form
    function validateForm(){
+
+      let pilotInput = document.getElementById('pilotName');
+      let copilotInput = document.getElementById('copilotName');
+      let fuelLevelInput = document.getElementById("fuelLevel");
+      let cargoMassInput = document.getElementById("cargoMass");
+      
+      let pilotStatus = document.getElementById('pilotStatus');
+      let copilotStatus = document.getElementById('copilotStatus');
+      let fuelStatus = document.getElementById('fuelStatus');
+      let cargoStatus = document.getElementById('cargoStatus');
+      let launchStatus = document.getElementById('launchStatus');
+   
+      let itemStatus = document.getElementById('itemStatus');
       
          
-         let pilotInputValue = String(pilotInput.value)
-         let pilotInputCheck;
+         let pilotInputValue = String(pilotInput.value);
 
-         let copilotInputValue = String(copilotInput.value)
-         let copilotInputCheck;
+         let copilotInputValue = String(copilotInput.value);
  
-         let fuelLevelInputValue = parseFloat(fuelLevelInput.value)
-         let fuelLevelInputCheck;
+         let fuelLevelInputValue = parseFloat(fuelLevelInput.value);
 
-         let cargoMassInputValue = parseFloat(cargoMassInput.value)
-         let cargoMassInputCheck;
+         let cargoMassInputValue = parseFloat(cargoMassInput.value);
+
+         let fuelCheck = false;
+         let cargoCheck = false;
   
          //Emput Field Check
          if(pilotInputValue === "" || copilotInputValue === "" || fuelLevelInputValue === "" || cargoMassInputValue === ""){
             alert("All fields are require")
+            return
          }
          //Pilot input Check
-         if(!isNaN(pilotInputValue) || pilotInputValue === "" ){
+         if(!isNaN(pilotInputValue)){
             alert("Please enter the pilot's name. ")
-            pilotInputCheck = false;
-         }else{
-            pilotInputCheck = true;
-            // pilotStatus.innerHTML = `${pilotInput.value} is ready for launch`
+            return
          }
          //Copilot input Check
-         if(!isNaN(copilotInputValue) || copilotInputValue === "" ){
+         if(!isNaN(copilotInputValue)){
             alert("Please enter the copilot's name.")
-            copilotInputCheck = false;
-         }else{
-            copilotInputCheck = true;
-            // copilotStatus.innerHTML = `${copilotInput.value} is ready for launch`
+            return
          }
          
          
-         if(fuelLevelInputValue === "" ){
+         if(isNaN(fuelLevelInputValue)){
             alert("Please enter a number for fuel.")
-            fuelLevelInputCheck = false;
-         }else if(isNaN(fuelLevelInputValue)){
-            alert("Please enter a number for fuel.")
-            fuelStatus.innerHTML = ` `;
-            fuelLevelInputCheck = false;
+            return
          }else if(fuelLevelInput.value < 10000){
-            alert(`NOT ENOUGH FUEL FOR SHUTTLE TO TAKE OFF!`);
-            launchStatus.innerHTML = `Shuttle not ready for launch`
+            fuelStatus.innerHTML = `NOT ENOUGH FUEL FOR SHUTTLE TO TAKE OFF!`;
          }else{
-            // fuelStatus.innerHTML = `Fuel Level Check: Passed`;
-            fuelLevelInputCheck = true
-         }
-
-
-         if(cargoMassInputValue === "" ){
-            alert("Please enter a number for cargo mass.")
-            cargoMassInputCheck = false
-         } else if(isNaN(cargoMassInputValue)){
-            alert("Please enter a number cargo mass.")
-            cargoStatus.innerHTML = ` `;
-            cargoMassInputCheck = false;
-         }else if(cargoMassInput.value > 10000 || cargoMassInput.value < 0){
-            alert(`TOO MUCH MASS FOR SHUTTLE TO TAKE OFF!`);
-            launchStatus.innerHTML = `Shuttle not ready for launch`
-         }else{
-            // cargoStatus.innerHTML = `Cargo Mass Check: Passed`;
-            cargoMassInputCheck = true
+         fuelCheck = true;
+         fuelStatus.innerHTML = `Fuel Level Check: Passed`;
          }
          
-         if(pilotInputCheck === true && copilotInputCheck === true && fuelLevelInputCheck === true && cargoMassInputCheck === true){
-         pilotStatus.innerHTML = `${pilotInput.value} is ready for launch`
-         copilotStatus.innerHTML = `${copilotInput.value} is ready for launch`
-         fuelStatus.innerHTML = `Fuel Level Check: Passed`;
-         cargoStatus.innerHTML = `Cargo Mass Check: Passed`;
-         launchStatus.innerHTML = `Shuttle is ready for launch`
-         launchStatus.style.color = "green"
+         
+         
+         if(isNaN(cargoMassInputValue)){
+            alert("Please enter a number cargo mass.")
+            
+            return
+         }else if(cargoMassInput.value > 10000 || cargoMassInput.value < 0){
+            cargoStatus.innerHTML = `TOO MUCH MASS FOR SHUTTLE TO TAKE OFF!`;
          }else{
-            pilotStatus.innerHTML = ``
-         copilotStatus.innerHTML = ``
-         fuelStatus.innerHTML = ``;
-         cargoStatus.innerHTML = ``;
-            launchStatus.innerHTML = `Shuttle is not ready for launch` 
+            cargoCheck = true;
+            cargoStatus.innerHTML = `Cargo Mass Check: Passed`;
+         }
+
+         if(fuelCheck && cargoCheck){
+            // pilotStatus.innerHTML = `${pilotInput.value} is ready for launch`
+            pilotStatus.innerHTML = pilotInput.value
+            copilotStatus.innerHTML = `${copilotInput.value} is ready for launch`
+            launchStatus.style.color = "green"
+            launchStatus.innerHTML = `Shuttle is ready for launch`
+         }else{
+            launchStatus.innerHTML = `Shuttle is not ready for launch`
             launchStatus.style.color = "red"
          }
+
+         itemStatus.style.visibility= "visible"
 
    }//End of Function to validate input
    
